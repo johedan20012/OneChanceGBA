26b83619278ea3314f6ecfb9bdf9682308f4354d
Luis Kevin Johedan Ramírez Fuentes <johedan20012@gmail.com>
2026-03-30 15:41:13 -0600

    Finish city day 1

--- include/rooms/city.h
+++ include/rooms/city.h
@@ -4,13 +4,36 @@
 #include "bn_regular_bg_items_bg_city.h"
 
 #include "bn_sprite_ptr.h"
+#include "bn_sprite_palette_ptr.h"
 #include "bn_sprite_items_car_small.h"
+#include "bn_sprite_items_city_people.h"
 
 #include "room.h"
+#include "moveable.h"
 
 namespace game{
 class City : public Room{
 private:
+    static constexpr bn::color pal1[16] = {bn::color(0,0,0),bn::color(0,0,0),bn::color(14,19,26),bn::color(0,19,0),bn::color(31,24,1),bn::color(8,8,8),bn::color(15,15,15)};
+    static constexpr bn::color pal2[16] = {bn::color(0,0,0),bn::color(0,0,0),bn::color(14,19,26),bn::color(19,0,0),bn::color(31,24,1),bn::color(8,8,8),bn::color(15,15,15)};
+
+    bn::sprite_ptr small_npc[8];
+    int y_level_indx = 0;
+    static constexpr bn::fixed y_level_npc[8][8] = 
+        {
+         {33,33,33,33,32,32,32,32}, //+ 3
+         {34,34,34,34,33,33,33,33}, //- 7
+         {35,35,35,35,34,34,34,34}, //- 5
+         {40,40,40,40,39,39,39,39}, //- 4
+         {40,40,40,40,39,39,39,39}, //- 6 
+         {41,41,41,41,40,40,40,40}, //+ 1 
+         {42,42,42,42,41,41,41,41}, //+ 0
+         {42,42,42,42,41,41,41,41}, //+ 2
+        };
+    static constexpr bn::fixed dir_npc[8] = {0.7,-0.7,-0.7,-0.7,-0.7,0.7,0.7,0.7};
+
+    bn::sprite_ptr npc_cars[3];
+
     bn::sprite_ptr car;
 
 public:
--- src/rooms/city.cpp
+++ src/rooms/city.cpp
@@ -3,7 +3,18 @@
 namespace game{
 City::City(Player& _player, DIRECTION entering_from):
     Room(bn::regular_bg_items::bg_city.create_bg(8,48),bn::fixed_rect(0,0,240,160),_player),
+    small_npc{bn::sprite_items::city_people.create_sprite(4,33,3),bn::sprite_items::city_people.create_sprite(-100,0,7),bn::sprite_items::city_people.create_sprite(93,0,5),
+    bn::sprite_items::city_people.create_sprite(15,0,4),bn::sprite_items::city_people.create_sprite(-110,0,6),bn::sprite_items::city_people.create_sprite(124,40,1),
+    bn::sprite_items::city_people.create_sprite(78,41,0),bn::sprite_items::city_people.create_sprite(-36,42,2)},  // 0, + 74, +59 for cars pos 
+    npc_cars{bn::sprite_items::car_small.create_sprite(148,56),bn::sprite_items::car_small.create_sprite(222,56),bn::sprite_items::car_small.create_sprite(-55,56)},
     car(bn::sprite_items::car_small.create_sprite(0,0)){
+
+    npc_cars[0].set_palette(bn::sprite_palette_item(pal1,bn::bpp_mode::BPP_4));
+    npc_cars[0].set_horizontal_flip(true);
+    npc_cars[1].set_palette(bn::sprite_palette_item(pal2,bn::bpp_mode::BPP_4));
+    npc_cars[1].set_horizontal_flip(true);
+    npc_cars[2].set_palette(bn::sprite_palette_item(pal2,bn::bpp_mode::BPP_4));
+    npc_cars[2].set_horizontal_flip(true);
 
     player.setVisible(false);
     player.setMovementBox(bn::fixed_rect(0,0,280,160));
@@ -27,11 +38,31 @@ City::~City(){
 }
 
 bn::optional<RoomExit> City::update(){
+    for(auto& npc_car: npc_cars){
+        npc_car.set_x(npc_car.x()-0.70);
+        if(npc_car.x() < -168) npc_car.set_x(168);
+    }
+
+    for(int i = 0; i<8; i++){
+        auto& lil_npc = small_npc[i];
+        lil_npc.set_x(lil_npc.x() + dir_npc[i]);
+        if(dir_npc[i] > 0 && lil_npc.x() > 130) lil_npc.set_x(-130);
+        if(dir_npc[i] < 0  && lil_npc.x() < -130) lil_npc.set_x(130);
+        lil_npc.set_y(y_level_npc[i][y_level_indx]);
+    }
+    y_level_indx = (y_level_indx+1)%8;
+
     if(bn::keypad::right_held()){
         car.set_position(car.position().x()+1,car.position().y());
         player.setPos(player.getPos().x()+1,player.getPos().y());
     }
 
+    if(bn::keypad::start_pressed()){
+        BN_LOG("0:",small_npc[0].x(),"|",small_npc[0].y());
+        BN_LOG("4:",small_npc[4].x(),"|",small_npc[4].y());
+    }
+
+    
     Room::update();
 
     auto exit = checkExits();
