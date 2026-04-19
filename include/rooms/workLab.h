28e1f4a7ffbc3696aba0a51bbe475bc6aa75884f
Luis Kevin Johedan Ramírez Fuentes <johedan20012@gmail.com>
2026-04-18 20:02:39 -0600

    Finish work lab day 3

--- include/rooms/workLab.h
+++ include/rooms/workLab.h
@@ -3,6 +3,8 @@
 
 #include "bn_sprite_ptr.h"
 
+#include "bn_vector.h"
+
 #include "npc.h"
 #include "room.h"
 #include "timer.h"
@@ -14,12 +16,14 @@ private:
     Timer initial_dark; //0.9 seg, 54 frames
     Timer end_room; // 3.1 seg, 186 frames
 
-    NPC npcs[2];
+    bn::vector<NPC,3> npcs;
     bn::sprite_ptr pc_screen;
 
     GlobalVariables& global_var;
 
     void setVisibility(bool visibility);
+    void loadDay1_2();
+    void loadDay3();
 public:
     WorkLab(Player& _player,GlobalVariables& _global_var);
     ~WorkLab() override {}
--- src/rooms/workLab.cpp
+++ src/rooms/workLab.cpp
@@ -2,13 +2,13 @@
 
 #include "bn_sprite_items_pc_screen.h"
 #include "bn_sprite_items_work_people.h"
+#include "bn_sprite_items_work_people_b.h"
 
 #include "bn_regular_bg_items_bg_work_lab.h"
 
 namespace game{
 WorkLab::WorkLab(Player& _player,GlobalVariables& _global_var):
     Room(bn::regular_bg_items::bg_work_lab.create_bg(8,48),bn::fixed_rect(0,0,240,132),_player),
-    npcs{bn::sprite_items::work_people.create_sprite(-92,23,2),bn::sprite_items::work_people.create_sprite(-8,23,6)},
     pc_screen(bn::sprite_items::pc_screen.create_sprite(-28,17)),
     global_var(_global_var){
 
@@ -21,6 +21,16 @@ WorkLab::WorkLab(Player& _player,GlobalVariables& _global_var):
     player.setPos(60,23);
     player.setMovementBox(bn::fixed_rect(0,0,240,160));
 
+    switch(global_var.currentDay()){
+        case 3:
+            loadDay3();
+            break;
+        case 2:
+        default:
+            loadDay1_2();
+            break;
+    }
+
     setVisibility(false);
 }
 
@@ -28,12 +38,30 @@ void WorkLab::setVisibility(bool visibility){
     bg->set_visible(visibility);
     bg_paper->set_visible(visibility);
 
-    npcs[0].setVisibility(visibility);
-    npcs[1].setVisibility(visibility);
+    for(auto& npc : npcs){
+        npc.setVisibility(visibility);
+    }
 
     pc_screen.set_visible(visibility);
 
     player.setVisible(visibility);
+}
+
+void WorkLab::loadDay1_2(){
+    npcs.push_back(bn::sprite_items::work_people.create_sprite(-92,23,2));
+    npcs.push_back(bn::sprite_items::work_people.create_sprite(-8,23,6)); 
+}
+
+void WorkLab::loadDay3(){
+    npcs.push_back(bn::sprite_items::work_people_b.create_sprite(-110,22));
+    npcs.back().setHorizontalFlip(true);
+    npcs.push_back(bn::sprite_items::work_people.create_sprite(103,23,2));
+    npcs.push_back(bn::sprite_items::work_people.create_sprite(-48,23,5));
+    npcs.back().setHorizontalFlip(true);
+    npcs.back().getPalette().set_color(12,bn::color(6,3,2)); //Change hair color
+    npcs.back().getPalette().set_color(19,bn::color(0,0,0)); //Change shirt color
+
+    player.setPos(-13,23);
 }
 
 bn::optional<RoomExit> WorkLab::update(){
